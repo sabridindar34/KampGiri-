3df77b4dfba51ab2e3d7b17e72764c1b08fa730b
kosov <kosov@SABRI>
2022-10-17 21:10:58 +0300

    Proje dosyası ekle.

---
+++ KampGiriş/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi1 = sayi2;
+        sayi2 = 35;
+        Console.WriteLine(sayi1);
+
+        int[] sayilar1 = new int[] { 10, 20, 30 };
+        int[] sayilar2 = new int[] { 100, 200, 300 };
+        sayilar2= sayilar1;
+        sayilar2[0] = 35;
+        Console.WriteLine(sayilar1[0]);
+    }
+}
